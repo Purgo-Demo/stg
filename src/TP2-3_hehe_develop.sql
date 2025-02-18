8a0e300ccc3c56bf376dd68fdd2848c902db0ee9
Purgo AI <support@purgo.ai>
2025-02-18 05:58:25 -0500

    Commit message

---
+++ src/TP2-3_hehe_develop.sql
@@ -0,0 +1,10 @@
+/* 
+   SQL Query to view all data from the specified Unity Catalog table 
+   purgo_playground.purgo_playground.f_inv_movmnt 
+*/
+
+-- Use Databricks SQL syntax to query the data
+-- The table is accessed from the Unity Catalog
+
+SELECT *
+FROM purgo_playground.purgo_playground.f_inv_movmnt;
